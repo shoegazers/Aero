6f01bf2953d6642e5d4a1042d183cd52d8b72e32
Rivers <theriversfrost@gmail.com>
2026-02-17 23:31:20 +0000

    added the base of the visual keyboard

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,9 @@
-use std::num::NonZero;
 use eframe::egui;
+use egui::TextBuffer;
 
 fn main() -> eframe::Result {
     let options = eframe::NativeOptions {
-        viewport: egui::ViewportBuilder::default().with_inner_size([320.0, 240.0]),
+        viewport: egui::ViewportBuilder::default().with_inner_size([800.0, 600.0]),
         ..Default::default()
     };
     eframe::run_native(
@@ -29,26 +29,52 @@ impl Aero {
 impl eframe::App for Aero {
     fn update(&mut self, ctx: &egui::Context, frame: &mut eframe::Frame) {
 
-        if self.show_main_viewport {
-            egui::CentralPanel::default().show(ctx, |ui| {
-                ui.heading("Aero");
+        // let mut characters = "abcdefghijklmnopqrstuvwxyz1234567890";
+        let mut qwerty = "qwertyuiopasdfghjklzxcvbnm";
+        let mut label_responses: Vec<egui::Response> = Vec::new();
 
-                if ui.button("settings").clicked() {
-                    self.show_settings_viewport = true;
-                    self.show_main_viewport = false;
+        egui::CentralPanel::default().show(ctx, |ui|{
+            ui.label("Aero");
+
+            if ui.button("settings").clicked() {
+                self.show_settings_viewport = true;
+            }
+
+            egui::Grid::new("keys").show(ui, |ui|{
+                let chars: Vec<char> = qwerty.chars().collect();
+                let row_sizes = [10, 9, 7];
+
+                let mut index = 0;
+                for size in row_sizes {
+                    for ch in &chars[index..index + size] {
+                        let mut l = ui.label(ch.to_string());
+                        label_responses.push(l);
+                    }
+                    ui.end_row();
+                    index += size;
                 }
-            });
-        }
+            }); 
 
-        if self.show_settings_viewport {
-            egui::CentralPanel::default().show(ctx, |ui| {
-                ui.heading("settings");
-
-                if ui.button("back").clicked() {
-                    self.show_settings_viewport = false;
-                    self.show_main_viewport = true;
+            if ui.input(|i| i.key_pressed(egui::Key::A)) {
+                for l in label_responses {
+                    
                 }
-            });
-        }
+            }
+
+            /* 
+            for c in characters.chars() {
+                if ui.button(c.to_string()).clicked() {
+
+                }
+            }
+            */
+            if self.show_settings_viewport {
+                egui::Window::new("settings").show(ctx, |ui| {
+                    if ui.button("close").clicked() {
+                        self.show_settings_viewport = false;
+                    }
+                });
+            }
+        });
     }
 }
